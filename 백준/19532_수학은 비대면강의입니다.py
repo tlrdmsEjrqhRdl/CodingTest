3e8df357046cecb4937a945f61f15e5da79ecbbc
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2026-03-06 10:44:53 +0900

    class: 3월 6일 수업 마무리

--- 백준/19532_수학은 비대면강의입니다.py
+++ 백준/19532_수학은 비대면강의입니다.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline #system 모듈을 사용하는 것이 기존 input보
 a, b, c, d, e, f = map(int, input().split()); # 맵핑을 통해서 공백으로 a, b, c, d, e, f를 나누기 / 변수와 입력값의 갯수가 정확히 일치해야함
 
 x = (e * c - b * f) // (a * e - b * d) # x의 값을 계산하기 위해서 공식에 대입
-y = (a * f - e * d) // (a * e - b * d) # y의 값을 계산하기 위해서 공식에 대입
+y = (a * f - c * d) // (a * e - b * d) # y의 값을 계산하기 위해서 공식에 대입
 print(x, y);
 
 # 공식은 다음과 같다.
--- 백준/2839_설탕 배달.py
+++ 백준/2839_설탕 배달.py
@@ -1,11 +1,13 @@
 import sys
 input = sys.stdin.readline
 
+ans = -1
+
 n = int(input())
-for i in range (n // 5, 0, -1):
+for i in range (n // 5, -1, -1):
     remains = n - (5 * i)
     if remains % 3 == 0:
-        print(i + remains // 3)
+        ans = i + remains // 3
         break
-else:
-    print(-1)
+        
+print(ans if ans != -1 else -1)
